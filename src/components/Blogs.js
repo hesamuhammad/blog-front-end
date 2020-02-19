5c7b774b74fc2ef40794e5c97fd4afbe83ef6ce7
Hesa Muhammad <hesamilan@gmail.com>
2020-02-19 16:22:40 +0700

    add share button

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
-
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Row, Col, Button, Container } from "reactstrap";
 // import { Button, Row,Col } from 'antd';
 import { fetchDataBlogs } from "../actions";
 import moment from "moment";
+// import { Facebook, Twitter } from "react-sharingbuttons";
+// import "react-sharingbuttons/dist/main.css";
 class Blogs extends Component {
     componentDidMount = () => {
         this.props.fetchDataBlogs();
@@ -15,6 +16,8 @@ class Blogs extends Component {
     render() {
         const { data } = this.props;
         console.log(data, "data");
+        // const url = window.location.href;
+        // const shareText = "ayo dibuka website";
 
         return (
             <Container fluid={true}>
@@ -24,9 +27,7 @@ class Blogs extends Component {
                         const formatDate = moment(item.date).format(
                             "MMMM Do YYYY, h:mm:ss a"
                         );
-                        const shareButton = (
-                            <div class="addthis_inline_share_toolbox"></div>
-                        );
+
                         return (
                             <Col
                                 xs={12}
@@ -71,13 +72,19 @@ class Blogs extends Component {
                                         {item.body}
                                     </p>
 
-                                    {shareButton}
+                                    {/* <!-- Go to www.addthis.com/dashboard to customize your tools --> */}
+                                    <div className="addthis_inline_share_toolbox"></div>
 
-                                    <Row style={{ display: "flex" }}>
+                                    {/* <Row>
                                         <Col>
-                                            <Button type="dashed" size="large">
-                                                <i className="fa fa-comment"></i>
-                                            </Button>
+                                            <Facebook
+                                                url={url}
+                                                shareText={shareText}
+                                            />
+                                            <Twitter
+                                                url={url}
+                                                shareText={shareText}
+                                            />
                                         </Col>
                                         <Col>
                                             <Link to={`/blogs/id/:${item.id}`}>
@@ -89,7 +96,7 @@ class Blogs extends Component {
                                                 </Button>
                                             </Link>
                                         </Col>
-                                    </Row>
+                                    </Row> */}
                                 </Col>
                             </Col>
                         );
