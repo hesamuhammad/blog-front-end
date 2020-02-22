0f828ba955e077267d2aef3818fbfb1e1612181d
Hesa Muhammad <hesamilan@gmail.com>
2020-02-22 15:45:56 +0700

    add new page addblog

---
+++ src/pages/addblog.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class addblog extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Add New Blog here</h3>
+            </div>
+        );
+    }
+}
